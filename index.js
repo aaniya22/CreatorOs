fa9f1b29ba9bb401c4d0260b62117deeebdb4f0a
Aashutosh kumar bhardwaj <ashutoshkumarbhardwaj7@gmail.com>
2026-05-23 19:28:44 +0530

    fix vercel filesystem crash

--- index.js
+++ index.js
@@ -184,6 +184,8 @@ app.get('/u/:shortId', async (req, res) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+// });
+
+module.exports = app;
